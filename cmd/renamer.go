ea55571e9e7b1aece099d2be1e5835048413b147
Alex Kraasch <alex@kraasch.eu>
2025-02-17 01:05:43 +0100

    Courtesy flush.

--- cmd/renamer.go
+++ cmd/renamer.go
@@ -8,21 +8,15 @@ import (
 
   // basics.
   "fmt"
-  // "github.com/spf13/afero"
-  "os"
   "flag"
+
+  // file system.
+  "github.com/spf13/afero"
+  "os"
 
   // local packages.
   // rname "github.com/kraasch/renamer/pkg/rname"
 )
-
-// // Get the underlying os.FS
-// fs := os.FS
-// // Wrap it with Afero
-// aferoFs, err := afero.NewAfero(fs)
-// if err != nil {
-//   // handle error
-// }
 
 var (
   // return value.
@@ -39,6 +33,7 @@ var (
 type model struct {
   width     int
   height    int
+  fs        afero.Fs
 }
 
 func (m model) Init() tea.Cmd {
@@ -74,13 +69,16 @@ func (m model) View() string {
 
 func main() {
 
+  // Use operating system's file system, wrapped in afero.
+  fs := afero.NewOsFs()
+
   // parse flags.
   flag.BoolVar(&verbose,  "verbose",   false, "Show info")
   flag.BoolVar(&suppress, "suppress",  false, "Print nothing")
   flag.Parse()
 
   // init model.
-  m := model{0, 0}
+  m := model{0, 0, fs}
 
   // start bubbletea.
   if _, err := tea.NewProgram(m, tea.WithAltScreen()).Run(); err != nil {
@@ -95,124 +93,3 @@ func main() {
 
 } // fin.
 
-/* TODO: make this into a sub module, ie package. */
-  // "fmt"
-  // "bufio"
-  // "os"
-  // "os/exec"
-  // "strings"
-  // "log"
-
-// func manualRename() {
-//   // get the editor from the environment.
-//   editor := os.Getenv("EDITOR")
-//   if editor == "" {
-//     log.Fatal("EDITOR environment variable is not set.")
-//   }
-//   fmt.Printf("Using editor '%s'.\n", editor)
-// 
-//   // get the current directory.
-//   dir, err := os.Getwd()
-//   if err != nil {
-//     log.Fatal(err)
-//   }
-//   fmt.Printf("Renaming directory '%s'.\n", dir)
-// 
-//   // read the files in the current directory.
-//   files, err := os.ReadDir(dir)
-//   if err != nil {
-//     log.Fatal(err)
-//   }
-// 
-//   // filter the list of files to only include regular files (ignore directories).
-//   var fileNames []string
-//   for _, file := range files {
-//     if !file.IsDir() {
-//       fileNames = append(fileNames, file.Name())
-//     }
-//   }
-//   fmt.Printf("Files before: '%s'.\n", fileNames)
-// 
-//   // open an editor to edit the list of files.
-//   fileListFile, err := os.CreateTemp("", "file_list_*.txt")
-//   if err != nil {
-//     log.Fatal(err)
-//   }
-//   defer os.Remove(fileListFile.Name()) // cleanup the temp file.
-// 
-//   // write the file names to the temp file, one per line.
-//   writer := bufio.NewWriter(fileListFile)
-//   for _, fileName := range fileNames {
-//     writer.WriteString(fileName + "\n") // TODO: check error.
-//   }
-//   writer.Flush()
-// 
-//   // open the editor with the temp file.
-//   cmd := exec.Command(editor, fileListFile.Name())
-//   cmd.Stdout = os.Stdout
-//   cmd.Stderr = os.Stderr
-//   if err := cmd.Run(); err != nil {
-//     log.Fatalf("Error while launching the editor: %v", err)
-//   }
-// 
-//   // read the edited list of file names.
-//   fileListFile.Seek(0, 0) // TODO: check error.
-//   scanner := bufio.NewScanner(fileListFile)
-//   editedFileNames := []string{}
-//   intermFileNames := []string{}
-//   for scanner.Scan() {
-//     editedFileNames = append(editedFileNames, strings.TrimSpace(scanner.Text()))
-//   }
-//   if err := scanner.Err(); err != nil {
-//     log.Fatal(err)
-//   }
-//   fmt.Printf("Files after: '%s'.\n", editedFileNames)
-// 
-//   // handle renaming files carefully to avoid collisions.
-//   renamedFiles := make(map[string]string)
-//   for i, oldName := range fileNames {
-//     newName := editedFileNames[i]
-//     if newName == oldName {
-//       fmt.Printf("Nothing to do: %s == %s.\n", oldName, newName)
-//       continue
-//     }
-// 
-//     // check for filename collisions.
-//     if _, exists := renamedFiles[newName]; exists {
-//       // there is a collision, rename with an intermediate name.
-//       someHash := "b8b81ed4" // TODO: make this random
-//       intermediateName := fmt.Sprintf("%s_%d_%s", newName, i, someHash)
-//       fmt.Printf("Renaming %s to %s (intermediate)\n", oldName, intermediateName)
-//       if err := os.Rename(oldName, intermediateName); err != nil {
-//         log.Fatal(err)
-//       } else {
-//         intermFileNames = append(intermFileNames, intermediateName)
-//         fmt.Printf(" 1 - Renamed: %s to %s.\n", oldName, intermediateName)
-//       }
-//       renamedFiles[intermediateName] = oldName
-//       continue
-//     }
-// 
-//     // rename the file if there's no conflict.
-//     if err := os.Rename(oldName, newName); err != nil {
-//       log.Fatal(err)
-//     } else {
-//       fmt.Printf(" 2 - Renamed: %s to %s.\n", oldName, newName)
-//     }
-//     renamedFiles[newName] = oldName
-//   }
-// 
-//   // ensure that files that were renamed to intermediate names are renamed to their final names.
-//   for _, intermediateName := range intermFileNames {
-//     finalName := renamedFiles[intermediateName]
-//     if intermediateName != finalName {
-//       if err := os.Rename(intermediateName, finalName); err != nil {
-//         log.Fatal(err)
-//       } else {
-//         fmt.Printf(" 3 - Renamed: %s to %s.\n", intermediateName, finalName)
-//       }
-//     }
-//   }
-// 
-//   fmt.Println("Files renamed successfully.")
-// }
