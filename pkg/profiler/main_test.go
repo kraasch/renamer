adb62ef824bc5a272a9a3904055e349e63d2f34a
Alex Kraasch <alex@kraasch.eu>
2025-02-22 12:44:43 +0100

    Add more test stubs.

--- pkg/profiler/main_test.go
+++ pkg/profiler/main_test.go
@@ -17,6 +17,7 @@ func TestAll(t *testing.T) {
 }
 
 var suites = []gt.TestSuite{
+
   /*
   * Test for the ReadRawProfileConfig().
   */
@@ -53,6 +54,81 @@ var suites = []gt.TestSuite{
       },
     },
   },
+
+  /*
+  * Test for CreateProfile().
+  */
+  {
+    TestingFunction:
+    func(t *testing.T, in gt.TestList) string {
+      // wordSeparators := in.InputArr[0]
+      // deleteChars    := in.InputArr[1]
+      // smallGapMark   := in.InputArr[2]
+      // bigGapMark     := in.InputArr[3]
+      // conversions    := in.InputArr[4]
+      // modesString    := in.InputArr[5]
+      // targetName     := in.InputArr[6]
+      // p := pro.CreateProfile
+      output := "Toast"
+      return output
+    },
+    Tests:
+    []gt.TestList{
+      {
+        TestName: "xxx_00",
+        IsMulti:  false,
+        InputArr: []string{
+          " ()", // word separators.
+          "",    // delete characters.
+          "-",   // small gap replacement.
+          "_",   // big gap replacement.
+          "cAa", // list of actions.
+          "",    // string of modes.
+          "The Walking Dead S05E01 No Sanctuary (1080p x265 Joy).mkv",
+          "the-walking-dead-s05e01-no-sanctuary_1080p-x265-joy.mkv",
+        },
+        ExpectedValue: "false",
+      },
+    },
+  },
+
+  /*
+  * Test for AddProfileToConfig().
+  */
+  {
+    TestingFunction:
+    func(t *testing.T, in gt.TestList) string {
+      // wordSeparators := in.InputArr[0]
+      // deleteChars    := in.InputArr[1]
+      // smallGapMark   := in.InputArr[2]
+      // bigGapMark     := in.InputArr[3]
+      // conversions    := in.InputArr[4]
+      // modesString    := in.InputArr[5]
+      // targetName     := in.InputArr[6]
+      // p := pro.CreateProfile
+      output := "Toast"
+      return output
+    },
+    Tests:
+    []gt.TestList{
+      {
+        TestName: "xxx_00",
+        IsMulti:  false,
+        InputArr: []string{
+          " ()", // word separators.
+          "",    // delete characters.
+          "-",   // small gap replacement.
+          "_",   // big gap replacement.
+          "cAa", // list of actions.
+          "",    // string of modes.
+          "The Walking Dead S05E01 No Sanctuary (1080p x265 Joy).mkv",
+          "the-walking-dead-s05e01-no-sanctuary_1080p-x265-joy.mkv",
+        },
+        ExpectedValue: "false",
+      },
+    },
+  },
+
   /* Fin test suite. */
 }
 
