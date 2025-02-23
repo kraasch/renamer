1dc154700f3747e9ba47bbceda744cf4d910b1b7
Alex Kraasch <alex@kraasch.eu>
2025-02-23 22:03:22 +0100

    Courtesy flush.

--- pkg/fsmanage/main.go
+++ pkg/fsmanage/main.go
@@ -2,17 +2,24 @@
 package fsmanage
 
 import (
+  "fmt"
   "strings"
   "github.com/spf13/afero"
 )
 
 func DirRename(fileSystem afero.Fs, originalNames string, targetNames string) {
+  fmt.Println("TOAST ########################## ")
+  fmt.Println(originalNames)
+  fmt.Println("TOAST ########################## ")
+  fmt.Println(targetNames)
+  fmt.Println("TOAST ########################## ")
   origs  := strings.Split(originalNames, "\n")
   tagets := strings.Split(targetNames, "\n")
   for i := range origs {
     o := origs[i]
     t := tagets[i]
-    _ = fileSystem.Rename(o, t)
+    err := fileSystem.Rename(o, t)
+    fmt.Println(err)
   }
 }
 
