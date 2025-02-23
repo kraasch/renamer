ba77171c21db8bf78c021862f5e4140851375b88
Alex Kraasch <alex@kraasch.eu>
2025-02-23 19:15:55 +0100

    Add more AUTORN tests.

--- pkg/autorn/main_test.go
+++ pkg/autorn/main_test.go
@@ -24,8 +24,20 @@ var suites = []gt.TestSuite{
   {
     TestingFunction:
     func(t *testing.T, in gt.TestList) string {
-      toml := in.InputArr[0]
-      name := in.InputArr[1]
+      toml := `title = "My Conf"` + NL +
+              `` + NL +
+              `[profiles]` + NL +
+              `  [profiles.prettify-txt]` + NL +
+              `    name = "SomeProfile"` + NL +
+              `    [profiles.prettify-txt.profile_rule]` + NL +
+              `      word_separators = " ()"` + NL +
+              `      delete_chars = ""` + NL +
+              `      small_gap_mark = "-"` + NL +
+              `      big_gap_mark = "_"` + NL +
+              `      conversions = "cAa"` + NL +
+              `      modes_string = ""` + NL +
+              ``
+      name := in.InputArr[0]
       var auto AutoRenamer
       auto.Parse(toml)
       output := auto.ConvertWith("prettify-txt", name)
@@ -34,26 +46,79 @@ var suites = []gt.TestSuite{
     Tests:
     []gt.TestList{
       {
-        TestName: "autorn_apply-profile_00",
+        TestName: "autorn_apply-profile_single-line_file_00",
         IsMulti:  false,
         InputArr: []string{
-          `title = "My Conf"` + NL +
-          `` + NL +
-          `[profiles]` + NL +
-          `  [profiles.prettify-txt]` + NL +
-          `    name = "SomeProfile"` + NL +
-          `    [profiles.prettify-txt.profile_rule]` + NL +
-          `      word_separators = " ()"` + NL +
-          `      delete_chars = ""` + NL +
-          `      small_gap_mark = "-"` + NL +
-          `      big_gap_mark = "_"` + NL +
-          `      conversions = "cAa"` + NL +
-          `      modes_string = ""` + NL +
-          ``,
           "The Walking Dead S05E01 No Sanctuary (1080p x265 Joy).mkv",
         },
         ExpectedValue:
           "the-walking-dead-s05e01-no-sanctuary_1080p-x265-joy.mkv",
+      },
+      {
+        TestName: "autorn_apply-profile_multi-line_file_00",
+        IsMulti:  true,
+        InputArr: []string{
+          "The Walking Dead S05E01 No Sanctuary (1080p x265 Joy).mkv" + NL +
+          "The Walking Dead S05E01 No Sanctuary (1080p x265 Joy).mkv",
+        },
+        ExpectedValue:
+          "the-walking-dead-s05e01-no-sanctuary_1080p-x265-joy.mkv" + NL +
+          "the-walking-dead-s05e01-no-sanctuary_1080p-x265-joy.mkv",
+      },
+      {
+        TestName: "autorn_apply-profile_multi-line_path_00",
+        IsMulti:  true,
+        InputArr: []string{
+          "videos/The Walking Dead S05E01 No Sanctuary (1080p x265 Joy).mkv" + NL +
+          "videos/The Walking Dead S05E01 No Sanctuary (1080p x265 Joy).mkv",
+        },
+        ExpectedValue:
+          "videos/the-walking-dead-s05e01-no-sanctuary_1080p-x265-joy.mkv" + NL +
+          "videos/the-walking-dead-s05e01-no-sanctuary_1080p-x265-joy.mkv",
+      },
+      {
+        TestName: "autorn_apply-profile_multi-line_path_01",
+        IsMulti:  true,
+        InputArr: []string{
+          "./videos/The Walking Dead S05E01 No Sanctuary (1080p x265 Joy).mkv" + NL +
+          "./videos/The Walking Dead S05E01 No Sanctuary (1080p x265 Joy).mkv",
+        },
+        ExpectedValue:
+          "./videos/the-walking-dead-s05e01-no-sanctuary_1080p-x265-joy.mkv" + NL +
+          "./videos/the-walking-dead-s05e01-no-sanctuary_1080p-x265-joy.mkv",
+      },
+      {
+        TestName: "autorn_apply-profile_multi-line_path_02",
+        IsMulti:  true,
+        InputArr: []string{
+          "./Videos/The Walking Dead S05E01 No Sanctuary (1080p x265 Joy).mkv" + NL +
+          "./Videos/The Walking Dead S05E01 No Sanctuary (1080p x265 Joy).mkv",
+        },
+        ExpectedValue:
+          "./Videos/the-walking-dead-s05e01-no-sanctuary_1080p-x265-joy.mkv" + NL +
+          "./Videos/the-walking-dead-s05e01-no-sanctuary_1080p-x265-joy.mkv",
+      },
+      {
+        TestName: "autorn_apply-profile_multi-line_path_03",
+        IsMulti:  true,
+        InputArr: []string{
+          "./my videos/The Walking Dead S05E01 No Sanctuary (1080p x265 Joy).mkv" + NL +
+          "./my videos/The Walking Dead S05E01 No Sanctuary (1080p x265 Joy).mkv",
+        },
+        ExpectedValue:
+          "./my videos/the-walking-dead-s05e01-no-sanctuary_1080p-x265-joy.mkv" + NL +
+          "./my videos/the-walking-dead-s05e01-no-sanctuary_1080p-x265-joy.mkv",
+      },
+      {
+        TestName: "autorn_apply-profile_multi-line_path_04",
+        IsMulti:  true,
+        InputArr: []string{
+          "./my videos/The Walking Dead S05E01 No Sanctuary (1080p x265 Joy).mkv" + NL +
+          "./my Videos/The Walking Dead S05E01 No Sanctuary (1080p x265 Joy).mkv",
+        },
+        ExpectedValue:
+          "./my videos/the-walking-dead-s05e01-no-sanctuary_1080p-x265-joy.mkv" + NL +
+          "./my Videos/the-walking-dead-s05e01-no-sanctuary_1080p-x265-joy.mkv",
       },
     },
   },
