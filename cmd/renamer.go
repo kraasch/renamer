4f2f2969e388354739fe6a73015dc3ad7ade6ddc
Alex Kraasch <alex@kraasch.eu>
2025-02-24 22:40:34 +0100

    Interpret parsed arguments.

--- cmd/renamer.go
+++ cmd/renamer.go
@@ -30,7 +30,6 @@ import (
 //              ruleString = STRING
 //             profileName = STRING
 var args struct {
-  Suppress       bool   `arg:"-u"`
   Verbose        bool   `arg:"-v"`
 	InputMode      string `arg:"-i,required"`
 	ConversionMode string `arg:"-c,required"`
@@ -68,7 +67,8 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
   case tea.KeyMsg:
     switch msg.String() {
     case "enter":
-      fsm.DirRename(m.fs, "abc.txt", "xyz.txt")
+      {}
+      // fsm.DirRename(m.fs, "abc.txt", "xyz.txt") // TODO: remove.
     case "q":
       output = "You quit on me!"
       return m, tea.Quit
@@ -80,29 +80,37 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 func (m model) View() string {
   var str string
   str += fmt.Sprintf("Verbose:  %#v (%[1]T)\n", args.Verbose)
-  str += fmt.Sprintf("Suppress: %#v (%[1]T)", args.Suppress)
   str = styleBox.Render(str)
   return lip.Place(m.width, m.height, lip.Center, lip.Center, str)
 }
 
-func main() {
-
+func StartInteractiveGui(in string) string {
   // use operating system's file system, wrapped in afero.
   fs := afero.NewOsFs()
+  // init model.
+  m := model{0, 0, fs}
+  // start bubbletea.
+  if _, err := tea.NewProgram(m, tea.WithAltScreen()).Run(); err != nil {
+    fmt.Println("Error running program:", err)
+    os.Exit(1)
+  }
+  return output
+}
+
+func main() {
 
   // parse flags.
   p := arg.MustParse(&args)
+  // input.
   if args.InputMode != "pipe" && args.InputMode != "dir" && args.InputMode != "recursive" {
     p.Fail("InputMode must be one of {pipe,dir,recursive}")
   }
-  if args.ConversionMode != "rule" && args.ConversionMode != "profile" && args.ConversionMode != "editor" && args.ConversionMode != "interactive" {
-    p.Fail("ConversionMode must be one of {rule,profile,editor,interactive}")
-  }
-  if args.OutputMode != "apply" && args.OutputMode != "validate" {
-    p.Fail("OutputMode must be one of {apply,validate}")
-  }
   if (args.InputMode == "dir" || args.InputMode != "recursive") && args.SelectionMode == "" {
     p.Fail("InputModes {dir,recursive} need a SelectionMode {all,files,dirs}")
+  }
+  // conversion.
+  if args.ConversionMode != "rule" && args.ConversionMode != "profile" && args.ConversionMode != "editor" && args.ConversionMode != "interactive" {
+    p.Fail("ConversionMode must be one of {rule,profile,editor,interactive}")
   }
   if args.ConversionMode != "rule" && args.RuleString == "" {
     p.Fail("ConversionMode {rule} needs a RuleString argument")
@@ -110,19 +118,50 @@ func main() {
   if args.ConversionMode != "profile" && args.ProfileName == "" {
     p.Fail("ConversionMode {profile} needs a ProfileName argument")
   }
-
-  // init model.
-  m := model{0, 0, fs}
-
-  // start bubbletea.
-  if _, err := tea.NewProgram(m, tea.WithAltScreen()).Run(); err != nil {
-    fmt.Println("Error running program:", err)
-    os.Exit(1)
+  // output.
+  if args.OutputMode != "apply" && args.OutputMode != "validate" {
+    p.Fail("OutputMode must be one of {apply,validate}")
   }
 
-  // print the last highlighted value in calendar to stdout.
-  if !args.Suppress {
-    fmt.Println(output)
+  // read input.
+  input := ""
+  switch args.InputMode {
+  case "pipe":
+    input = ""
+    fmt.Println("Get input from pipe")
+  case "dir":
+    input = ""
+    fmt.Println("Get input dir")
+  case "recursive":
+    input = ""
+    fmt.Println("Get input recursive")
+  }
+
+  // make conversion.
+  conversion := ""
+  switch args.ConversionMode {
+  case "rule":
+    conversion = ""
+    fmt.Println("Convert by rule")
+  case "profile":
+    conversion = ""
+    fmt.Println("Convert by profile")
+  case "editor":
+    conversion = ""
+    fmt.Println("Convert by editor")
+  case "interactive":
+    conversion = StartInteractiveGui(input)
+    fmt.Println("Convert interactively")
+  }
+
+  // give output.
+  switch args.OutputMode {
+  case "apply":
+    fmt.Println("Give output by applying")
+    fmt.Println(conversion)
+  case "validate":
+    fmt.Println("Give output by validating")
+    fmt.Println(conversion)
   }
 
 } // fin.
